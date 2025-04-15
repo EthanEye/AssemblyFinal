74efbdb893148b837db56da69cf0599f3c887c43
Ethan Eye <ethan@ETHANS-G77700>
2025-04-15 13:29:37 -0600

    Test

--- ByteJumper/ByteJumper/Source Files/main.asm
+++ ByteJumper/ByteJumper/Source Files/main.asm
@@ -10,18 +10,22 @@ option casemap:none
 INCLUDE Irvine32.inc
 INCLUDELIB Irvine32.lib
 
+
+
 .data
-msg db "Hello, world!", 0
+
+
 .code
 main PROC
-_main:
-    mov edx, OFFSET msg
-    call WriteString
-    call Crlf
-    push 0
-    call ExitProcess       ; use Irvine32's wrapped version
+  
+   
 
+ 
+      
+    push 0
+    call ExitProcess
 main ENDP
+
 
 END
 
