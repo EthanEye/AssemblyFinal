79c02b52d7fd0430794924edee681faf44b39636
Ethan Eye <ethan@ETHANS-G77700>
2025-04-14 22:07:41 -0600

    Irvine

--- ByteJumper/ByteJumper/Source Files/main.asm
+++ ByteJumper/ByteJumper/Source Files/main.asm
@@ -1,35 +1,27 @@
-;----------------- Copy from here ---------------
+
 ; MASM Template
 ; <EthanEye>
 ; 3/6/2025
 ; Create a template for assembler files.
 .386P
-.model flat
 
-extern   _ExitProcess@4: near
-extern writeNumber: near
-extern initialize_console: near
+option casemap:none
+
+INCLUDE Irvine32.inc
+INCLUDELIB Irvine32.lib
 
 .data
-
-fib1 dd 0 ; init fib 1
-fib2 dd 1 ; init fib 2
-count dd 0 ;
- 
-
+msg db "Hello, world!", 0
 .code
-main PROC near
+main PROC
 _main:
-
-call initialize_console
-
-
-
-	push 0
-	call	_ExitProcess@4
+    mov edx, OFFSET msg
+    call WriteString
+    call Crlf
+    push 0
+    call ExitProcess       ; use Irvine32's wrapped version
 
 main ENDP
 
-
 END
-;----------------- End Copy here ---------------
+
