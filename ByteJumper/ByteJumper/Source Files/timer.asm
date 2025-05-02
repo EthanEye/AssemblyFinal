1a4ddc171521b08895f1aadcba97e432e3666fa9
Ryan-P-Craig <Rcraig38@student.cccs.edu>
2025-05-01 18:02:20 -0600

    timer with minutes

--- ByteJumper/ByteJumper/Source Files/timer.asm
+++ ByteJumper/ByteJumper/Source Files/timer.asm
@@ -1,33 +1,58 @@
 INCLUDE Irvine32.inc
 INCLUDELIB Irvine32.lib
+
 .data
-timerDisplay     BYTE "TIME: ", 0
-digits       BYTE 12 DUP(0)
-timerCounter DWORD 0
+timerDisplay     BYTE "Time: ", 0
+clearSpaceStr    BYTE "             ", 0    ; enough to overwrite entire old text
+digits           BYTE 12 DUP(0)
+timerCounter     DWORD 0
 
 .code
 Timer PROC 
 
+    ; === Set text color to green ===
     mov eax, green
     call SetTextColor
-    ; Timer location
+
+    ; Clear previous timer output
     mov dh, 0
-    mov dl, 20
+    mov dl, 0
+    call Gotoxy
+
+    mov edx, OFFSET clearSpaceStr
+    call WriteString
+
+    ; Move to timer position again
+    mov dh, 0
+    mov dl, 0
     call Gotoxy
 
     ; Print "Time: "
     mov edx, OFFSET timerDisplay
     call WriteString
 
-    ; Print counter
+    ; Convert timerCounter into minutes and seconds
     mov eax, timerCounter
+    xor edx, edx
+    mov ecx, 60
+    div ecx            ; EAX = minutes, EDX = seconds
+
+    push edx           ; save seconds
+    call WriteDec      ; print minutes
+
+    mov al, ':'        ; print colon
+    call WriteChar
+
+    pop eax            ; restore seconds
     call WriteDec
 
+    ; Reset text color to green
     mov eax, white
     call SetTextColor
-   
+
+    ; Increment counter 
     inc timerCounter
 
     ret
 Timer ENDP
-END timer
+END Timer
